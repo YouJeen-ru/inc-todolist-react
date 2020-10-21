c2a0845dcb97c67fc267c8858912afb0c9ffcb75
YouJeen-ru <1.rouch@mail.ru>
2020-10-21 19:08:25 +0300

    Update new components

--- src/App.tsx
+++ src/App.tsx
@@ -27,11 +27,15 @@ function App() {
     let [tasks, setTasks] = useState<TasksStateType>({
         [todolistId1]: [
             {id: v1(), title: "HTML&CSS", isDone: true},
-            {id: v1(), title: "JS", isDone: true}
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "React", isDone: false},
+            {id: v1(), title: "Git", isDone: true},
         ],
         [todolistId2]: [
             {id: v1(), title: "Milk", isDone: true},
-            {id: v1(), title: "React Book", isDone: true}
+            {id: v1(), title: "React Book", isDone: false},
+            {id: v1(), title: "Beer", isDone: true},
+            {id: v1(), title: "Car", isDone: false},
         ]
     });
 
