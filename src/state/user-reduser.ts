406581a566f361997447e07e0f42d08e0ee9849e
YouJeen-ru <1.rouch@mail.ru>
2020-11-02 16:10:16 +0300

    immutable user-reducer up

--- src/state/user-reduser.ts
+++ src/state/user-reduser.ts
@@ -12,11 +12,9 @@ export type ActionType = {
 export const userReducer = (state: StateType, action: ActionType) => {
     switch (action.type) {
         case 'INCREMENT-AGE':
-            state.age = state.age + 1;
-            return state;
+            return {...state ,age: state.age + 1 };
         case 'INCREMENT-CHILDREN-COUNT':
-            state.childrenCount = state.childrenCount + 1;
-            return state;
+            return {...state, childrenCount:state.childrenCount + 1  };
         default:
             throw new Error("I don't understand this type")
     }
