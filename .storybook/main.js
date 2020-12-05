beac8901ec2317070e6d24343e9380e9b257b947
YouJeen-ru <1.rouch@mail.ru>
2020-12-05 12:55:16 +0300

    add axios on project

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,7 @@
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  // stories: ['../src/**/*.stories.tsx'],
+  stories: ['../src/stories/**/*.stories.tsx'],
+
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
